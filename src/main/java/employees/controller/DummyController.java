cb749fc506cdfd2c10032fcde9e9ad22cc13f066
Edgar Heredia <EdgarHeredia.Zenta@latam.com>
2020-05-02 02:20:12 -0400

    Fix sonar vulnerabilities in DummyController

--- src/main/java/employees/controller/DummyController.java
+++ src/main/java/employees/controller/DummyController.java
@@ -2,8 +2,7 @@ package employees.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -19,7 +18,7 @@ public class DummyController {
 	    this.dummyService = dummyService;
 	  }
 	
-	@RequestMapping(value = "/employees", method = RequestMethod.GET)
+	@GetMapping(path = "/employees")
 	public ResponseEntity<Object> getEmployees() {
 		String employees = dummyService.employees();
 	    if (employees != null) {
